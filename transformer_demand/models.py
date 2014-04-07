dd827d438c2633694c4e4b402591fb428b232347
Reid Miller <r24mille@uwaterloo.ca>
2014-04-07 18:33:29 -0400

    Refactored project into two apps. The original project's models.py and run.py files were placed into transformer_demand app.

---
+++ transformer_demand/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+
+class Transformer(models.Model):
+    """
+    Represents a distribution transformer
+    """
+    TransformerID = models.CharField(max_length=20, primary_key=True)
+    Type = models.CharField(max_length=100)
+    KVA = models.IntegerField()
+    Phasing = models.CharField(max_length=6)
+    CircuitNumber = models.CharField(max_length=16)
+    PrimaryVoltage = models.CharField(max_length=16)
+    SecondaryVoltage = models.CharField(max_length=20)
+    InstallationYear = models.IntegerField()
+    DWGNumber = models.CharField(max_length=30)
+    Phases = models.CharField(max_length=24)
+    Serial1 = models.CharField(max_length=100)
+    Serial2 = models.CharField(max_length=100)
+    Serial3 = models.CharField(max_length=100)
+    Enabled = models.BooleanField()
+    Quantity = models.SmallIntegerField()
+    Angle = models.DecimalField()
+    AreaTown = models.CharField(max_length=40)
+    Status = models.CharField(max_length=100)
+    Owner = models.CharField(max_length=100)
+    Connect_Date = models.TimeField(auto_now=False, auto_now_add=False)
+    Disconnect_Date = models.TimeField(auto_now=False, auto_now_add=False)
+    
+    class Meta:
+        db_table = "Transformers"
+        managed = False
+
+
+class TransformerLoad(models.Model):
+    """
+    A timeseries measurement of demand for each :model:`transformer_demand.Transformer`.
+    """
+    TransformerLoadID = models.IntegerField(primary_key=True, db_column="TransformerLoads_id")
+    Transformer = models.ForeignKey(Transformer, db_column="TransformerID")
+    ReadDate = models.DateField()
+    reading_datetime_standard = models.DateTimeField()
+    Interval = models.IntegerField()
+    LoadMW = models.FloatField(db_column="TransformerLoad")
+    
+    class Meta:
+        db_table = "TransformerLoads"
+        managed = False
+        ordering = ["reading_datetime_standard"]
+    
+
+class Meter(models.Model):
+    """
+    Represents a customer meter record
+    """
+    MeterID = models.IntegerField(primary_key=True)  # Although MySQL contains composite PK, MeterID is unique for Django
+    MeterNumber = models.CharField(max_length=30)
+    CustomerPremiseNumber = models.IntegerField()
+    ConnectDate = models.TimeField(auto_now=False, auto_now_add=False)
+    Transformer = models.ForeignKey(Transformer, db_column="TransformerID")
+
+    class Meta:
+        db_table = "Meters"
+        unique_together = ("MeterNumber", "CustomerPremiseNumber", "ConnectDate")
+        managed = False
+        
---
+++ transformer_demand/run.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import os
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demand_heatmap.settings")
+    
+    # Get timeseries of TransformerLoad associated with a Transformer
+    from transformer_demand.models import Transformer
+    phaseStr = "1"
+    areaStr = "A1-TEC"
+    transformers = Transformer.objects.using('ldc').filter(Phases=phaseStr, Enabled=True, AreaTown=areaStr)
+    
+    fignum = 1
+    for transformer in transformers:
+        demands = transformer.transformerload_set.all()
+        
+        # Transformer must have TransformerLoad items
+        if len(demands) > 0:
+            # Find the length of the results to know first and last index, then get the number of days
+            num_loads = len(demands)
+            start_date = demands[0].ReadDate
+            end_date = demands[num_loads - 1].ReadDate
+            num_days = (end_date - start_date).days
+        
+            # Create an n x 24 matrix to hold each hour's values and zeros for hours without value
+            load_heatmap = np.zeros(((num_days + 1), 24))
+            
+            # Loop over all TransformerLoad rows returned and populate them into the proper array slot
+            for zonalDemand in demands:
+                d = (zonalDemand.ReadDate - start_date).days
+                load_heatmap[d][zonalDemand.Interval] = zonalDemand.LoadMW
+            
+            # Plot the timeseries matrix as a heatmap
+            start_datenum = matplotlib.dates.date2num(start_date)
+            end_datenum = matplotlib.dates.date2num(end_date)
+            fig = plt.figure(fignum)
+            ax = plt.subplot()
+            plt.subplots_adjust(left=0.2, bottom=None, right=1, top=None,
+                wspace=None, hspace=None)
+            im = ax.imshow(load_heatmap, interpolation='none', aspect='auto', extent=(0, 24, start_datenum, end_datenum), origin='lower')
+            ax.yaxis_date()
+            ax.set_title("Transformer ID: " + transformer.TransformerID)
+            ax.set_xlabel("Hour of Day")
+            ax.set_ylabel("Day of Year")
+            ax.xaxis.set_ticks(range(0, 24, 2))
+            cb = fig.colorbar(im)
+            cb.set_label("Kilowatt-hours (kWh)")
+            plt.savefig("./figures/" + phaseStr + "-phase/" + areaStr + "/" + transformer.TransformerID + ".png", dpi=100)
+            plt.close(fignum)
+            
+            # Increase fignum
+            fignum = fignum + 1
+    
+    plt.show()
